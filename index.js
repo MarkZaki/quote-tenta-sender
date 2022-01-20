20a9f3b72fc08cfe3170165435745f6d48eacb5e
Mark <markzaky18@gmail.com>
2022-01-20 11:19:17 +0200

    try catch blocks

--- index.js
+++ index.js
@@ -4,25 +4,41 @@ const cron = require("node-cron");
 const express = require("express");
 
 const sendToTenta = (author, quote) => {
-  request.post(`https://tenta.me/${process.env.TENTA_CODE}`, {
-    json: { title: author, description: quote, options: {} },
-  });
+  try {
+    request.post(`https://tenta.me/${process.env.TENTA_CODE}`, {
+      json: { title: author, description: quote, options: {} },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const GetRandomQuote = () => {
-  request.get("https://api.quotable.io/random", {}, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-      const { content, author } = JSON.parse(body);
-      sendToTenta(author, content);
-    }
-  });
+  try {
+    request.get(
+      "https://api.quotable.io/random",
+      {},
+      (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+          const { content, author } = JSON.parse(body);
+          sendToTenta(author, content);
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const MyDailyQuote = () => {
-  sendToTenta(
-    "Daily Quote",
-    "Your Choices is What You Are, Not Your Money Or Status. and Good Morals Are Always Rewarded"
-  );
+  try {
+    sendToTenta(
+      "Daily Quote",
+      "Your Choices is What You Are, Not Your Money Or Status. and Good Morals Are Always Rewarded"
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const app = express();
